5d62e8ccfc669c928a21b128b13da332e4e59a91
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-09-25 20:05:53 +0530

    4 inputs

--- neuron.py
+++ neuron.py
@@ -1,6 +1,6 @@
-inputs = [1.2, 3.6, 8.2]
-weights = [3.2, 2.4, 5.6]
-bias = 3
+inputs = [1, 2, 3, 2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2
 
-output = (inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2]) + bias
+output = (inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + inputs[3] * weights[3]) + bias
 print(output)
