1542ded691f2b57217bf7d1b817285243ed60d24
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-09-25 20:30:00 +0530

    use np.dot

--- neuron.py
+++ neuron.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 inputs = [1, 2, 3, 2.5]
 
 weights = [
@@ -6,19 +8,8 @@ weights = [
     [-0.26, -0.27, 0.17, 0.87],
 ]
 
-biases = [
-    2,
-    3,
-    0.5,
-]
+biases = [2, 3, 0.5]
 
-layer_outputs = []
+output = np.dot(weights, inputs) + biases
 
-for neuron_weights, neuron_bias in zip(weights, biases):
-    neuron_output = 0
-    for n_input, weight in zip(inputs, neuron_weights):
-        neuron_output += n_input * weight
-    neuron_output += neuron_bias
-    layer_outputs.append(neuron_output)
-
-print(layer_outputs)
+print(output)
