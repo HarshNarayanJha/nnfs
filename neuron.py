517c13fc73291d432dd276f294cf342bf0433910
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-09-25 21:03:10 +0530

    Layer_Dense object and layers, forwards

--- neuron.py
+++ neuron.py
@@ -1,28 +1,25 @@
 import numpy as np
 
-inputs = [
+np.random.seed(0)
+
+X = [
     [1.0, 2.0, 3.0, 2.5],
     [2.0, 5.0, -1.0, 2.0],
     [-1.5, 2.7, 3.3, -0.8],
 ]
 
-weights = [
-    [0.2, 0.8, -0.5, 1.0],
-    [0.5, -0.91, 0.26, -0.5],
-    [-0.26, -0.27, 0.17, 0.87],
-]
+class Layer_Dense:
+    def __init__(self, n_inputs: int, n_neurons: int):
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
-biases = [2.0, 3.0, 0.5]
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-weights2 = [
-    [0.1, -0.14, 0.5],
-    [-0.5, 0.12, -0.33],
-    [-0.44, 0.73, -0.13],
-]
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
 
-biases2 = [-1, 2, -0.5]
+layer1.forward(X)
+layer2.forward(layer1.output)
 
-layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
-
-print(layer2_outputs)
+print(layer2.output)
