1d783e1e912ef4f08b2cb60446aaf00960c0ce92
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-09-25 22:27:19 +0530

    spiral_data utility and activation ReLU

--- neuron.py
+++ neuron.py
@@ -1,12 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+from utils import spiral_data
 
 np.random.seed(0)
 
-X = [
-    [1.0, 2.0, 3.0, 2.5],
-    [2.0, 5.0, -1.0, 2.0],
-    [-1.5, 2.7, 3.3, -0.8],
-]
+X, y = spiral_data(100, 3)
 
 class Layer_Dense:
     def __init__(self, n_inputs: int, n_neurons: int):
@@ -16,10 +15,15 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
 
 layer1.forward(X)
-layer2.forward(layer1.output)
 
-print(layer2.output)
+activation1.forward(layer1.output)
+
+print(activation1.output)
