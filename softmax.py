98da449e057de2e4f53ffa2e402f56c69b0d2fff
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-09-29 11:53:51 +0530

    correct use of numpy, axis and keepdims

--- softmax.py
+++ softmax.py
@@ -5,14 +5,24 @@
 #
 # Solution, exponentiation [exp(x)], it ALWAYS gives +ve output, even for -ve numbers, so we do have a scale with info now!
 
+# SoftMax Activation: Input -> Exp -> Normalize -> Output
+# SoftMax Activation: Input -> Softmax -> Output
+#
+# But exponentiation comes with a cost, for large values it just explodes, with exp(1000) being just overflowed too much
+# We can solve this by taking the largest input value and making it 0 (subtrating that value from each value)
+
 import numpy as np
 
-layer_outputs = [4.8, 1.21, 2.385]
+layer_outputs = [
+    [4.8, 1.21, 2.385],
+    [8.9, -1.81, 0.2],
+    [1.41, 1.051, 0.026],
+]
 
 # Exponentiation
 exp_values = np.exp(layer_outputs)
 print(exp_values)
 
 # Normalize them
-norm_values = exp_values / np.sum(exp_values)
-print(norm_values, sum(norm_values))
+norm_values = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+print(norm_values, np.sum(norm_values, axis=1))
