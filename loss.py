b43d69202fcaca80c75915494a58cd0b152eae4f
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-09-29 12:08:56 +0530

    loss - logs

---
+++ loss.py
@@ -0,0 +1,17 @@
+# We need to find the loss of the training in order to improve the model
+# Why not success but loss, because we need to correct those only.
+# Categorical Cross-Entropy
+
+'''
+Solving for x
+
+e ** x = b
+'''
+
+import numpy as np
+import math
+
+b = 5.2
+
+print(np.log(b))    # 1.6486586255873816
+print(math.e ** 1.6486586255873816) # 5.2
